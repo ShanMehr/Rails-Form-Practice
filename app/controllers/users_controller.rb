7d6c0a86d18b9d6bd5b9c87c57ff8014852e0bb8
ShanMehr <ishanmeher25@gmail.com>
2021-01-06 17:05:50 -0800

    Add User From Form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
-    def new
+# app/controllers/users_controller.rb
+def create
+    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+  
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
+  end
 
-    def create
-    end
+  def new
+    @user=User.new
+  end
+
+  def whitelisted_post_params
+    params.require(:user).permit(:username,:email,:password)
+  end
 end
